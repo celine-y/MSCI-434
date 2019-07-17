bb5985acf23f91cee2fe7cb3b5a4bd34f8c5c4b0
celine-y <clubs43v@gmail.com>
2019-07-17 17:36:25 -0400

    solution

--- Project/Formulation_1.py
+++ Project/Formulation_1.py
@@ -15,8 +15,8 @@ rc_order = pd.read_csv('Data - order_rc 2.csv', index_col='Type A/Type K')
 x_bar = pd.read_csv('Data - x_bar 2.csv', index_col='Type k').iloc[[12]].values[0]
 
 k_a = rc_order.values
-print(k_a)
-# Containts
+
+# Constants
 k = 17
 a= 41
 m = 12
@@ -48,7 +48,6 @@ for i in range(0, k): #k
     for j in range(0, m): #m
         model.addConstr(quicksum(k_a[h, i]*y[h, i] for h in range(0, a)) == n[j, i])
         #model.addConstr(quicksum(y[h, i] for h in range(0, a)) == n[j, i])
-        
 
 # 4)
 for i in range(0, k): #k
@@ -63,5 +62,60 @@ model.update()
 model.optimize()
 
 for g in model.getVars():
+    if g.x > 0:
+        print(g.varName, g.x)
+
+
+# Formulation 2
+        
+#Data
+rc_order = pd.read_csv('Data - order_rc 2.csv', index_col='Type A/Type K')
+c = pd.read_csv('Data - rc_cost.csv', index_col='Type K')['Cost per month'].values
+
+T = model.objVal
+        
+# Create model
+m2 = Model()
+
+# Variables
+n = m2.addVars(m, k, vtype=GRB.INTEGER, name='n') #n_mk
+x = m2.addVars(k, vtype=GRB.INTEGER, name='x') #x_k
+y = m2.addVars(a, k, vtype=GRB.BINARY, name='y') #y_ak
+z = m2.addVars(k, vtype=GRB.BINARY, name='z') #z_k
+
+# Objective Function
+m2.setObjective(quicksum(c[i]*x[i] for i in range(0, k)), GRB.MINIMIZE)
+ 
+# 1)
+for i in range(0, a):
+    m2.addConstr(quicksum(k_a[i, j]*y[i, j] for j in range(0, k)) == 1)
+    
+# 2)
+for i in range(0, k): #K
+    for j in range(0, a): #A
+        m2.addConstr(z[i] >= y[j, i])
+        
+# 3)
+for i in range(0, k): #k
+    for j in range(0, m): #m
+        m2.addConstr(quicksum(k_a[h, i]*y[h, i] for h in range(0, a)) == n[j, i])
+
+# 4)
+for i in range(0, k): #k
+    for j in range(0, m): #m
+        m2.addConstr(n[j, i] <= x[i])
+
+# 5)
+for i in range(0, k): #k
+    m2.addConstr(x[i] <= x_bar[i])
+    
+# 6) NEW
+m2.addConstr(quicksum(z[i] for i in range(0, k)) <= T)
+     
+
+m2.update()
+m2.optimize()
+
+for g in m2.getVars():
     if g.x > 0:
         print(g.varName, g.x)
